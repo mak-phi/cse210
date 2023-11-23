550ebec306b124188e74d3fd38da4b1d420845f4
mak-phi <mkmakiti@gmail.com>
2023-11-24 00:49:27 +0300

    w04 learning assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment1 = new MathAssignment("Vasily Zaytsev","Trigonometry","7.6","2-5");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetHomeworkList());
+
+        WritingAssignment assignment2 = new WritingAssignment("Morgan Freeman","The Middle Passage","The Development of Non-verbal Language Among Enslaved Africans");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
     }
 }
